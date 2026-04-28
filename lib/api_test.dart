ef9ba7b6a6e45933a3f9d20fa771f6f48cd7ec3c
Varghese John <kuk.varghese@gmail.com>
2026-04-28 22:30:52 +0530

    bug fixed

--- lib/api_test.dart
+++ lib/api_test.dart
@@ -1,21 +1,19 @@
 import 'dart:io';
-import 'package:dio/dio.dart';
 import 'package:flood_alert/services/water_api_service.dart';
-import 'package:flood_alert/models/water_reading.dart';
 
 void main() async {
   print('Starting API Test...');
   try {
     final api = WaterApiService();
     print('Fetching main station...');
-    final readings = await api.fetchLast24Hours();
+    final readings = await api.fetchReadings();
     print('Fetched ${readings.length} readings for main station.');
     if (readings.isNotEmpty) {
       print('Latest: ${readings.last.waterLevel} at ${readings.last.dataTime}');
     }
 
     print('Fetching upstream station...');
-    final upReadings = await api.fetchLast24Hours(stationCode: WaterStation.pullakkayar);
+    final upReadings = await api.fetchReadings(stationCode: WaterStation.pullakkayar);
     print('Fetched ${upReadings.length} readings for upstream station.');
     if (upReadings.isNotEmpty) {
       print('Latest: ${upReadings.last.waterLevel} at ${upReadings.last.dataTime}');
